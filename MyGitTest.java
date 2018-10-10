59ecb3a0145bbb4ff1677cf732489f97a90c30e2
John Meech <jmeech@carroll.edu>
2018-10-10 14:42:03 -0600

    Added 2's to branch 2

--- MyGitTest.java
+++ MyGitTest.java
@@ -2,7 +2,7 @@
 public class MyGitTest {
 
 public static void main(String[] args) {
-	System.out.println("John says Branch2 Hello Git Test");
+	System.out.println("Joh2n s222222222222222222222222ay2222 Br222anch2 He222222llo 2222Git2 22222 222Te2s2t222222222");
 }
 
 }
