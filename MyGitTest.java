77e920407773ae5f52fbc5dd0b5b61e320d10f69
John Meech <jmeech@carroll.edu>
2018-10-10 14:38:44 -0600

    reset Git Test to original

--- MyGitTest.java
+++ MyGitTest.java
@@ -2,7 +2,7 @@
 public class MyGitTest {
 
 public static void main(String[] args) {
-	System.out.println("John says Branch2 Hello Git Test");
+	System.out.println("John says Hello Git Test");
 }
 
 }
